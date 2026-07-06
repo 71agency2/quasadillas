c8f92038f98c55de93887ab0ddd3a917481d51a1
Mouad Zaidi <zaidimouad1@gmail.com>
2026-07-06 15:54:50 +0100

    Update category sorting to chronological order

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Pizza Delivery",
-  description: "Chaud, Rapide & Délicieux",
+  title: "Food Plus",
+  description: "Depuis 2011",
 };
 
 export default function RootLayout({
